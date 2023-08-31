0852c1ff64e51acc6b80b9046845bbe3e38b27ae
fay <110033961+Nabilafayza@users.noreply.github.com>
2023-08-31 20:58:52 +0700

    change jurnal fitur
    jurnal fitur

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,9 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="{{ asset('css/other.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('js/main.js') }}">
+
+    <script src="{{ asset('js/main.js') }}"></script>
   <title></title>
 </head>
 <body>
@@ -49,6 +52,10 @@
     @yield('content')
   <!-- Bootstrap JS and Custom JS -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="js/main.js"></script>
 </body>
 
 </html>
--- resources/views/fitur.blade.php
+++ resources/views/fitur.blade.php
@@ -6,5 +6,25 @@
         <span class="flaticon-shape"></span>
     </div>
 </div>
+<div id="content" class="container">
+    <div class="jumbotron bg-dark text-white">
+      <h1 class="display-4">Hello, User!</h1>
+      <hr class="my-4">
+      <p class="lead">Welcome to your daily jurnal, how do you feel today?</p>
+    </div>
+    
+    <div class="card">
+      <div class="card-body">
+        today is: <h4 id="activeDate">31 December 2023</h4>
+      </div>
+    </div>
+    <div class="right-section col-lg-6">
+        <br>
+        <section name="journal">
+          <div class="card">
+            <div class="card-header"><b>Journal</b></div>
+            <div id='journal-card' class="card-body">
+              <!-- start generation here-->
 
+                </form>
 @endsection
